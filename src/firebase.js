278492860d569ba2bc2ceb2b402ed73046d3f7fa
abhishek-aws <52865600+abhishek-aws@users.noreply.github.com>
2020-09-20 23:51:14 +0530

    firebase config updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,14 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAPifTJth3vw1KRugk2vudpYQB4cNezQQs",
-//   authDomain: "todo-app-381c4.firebaseapp.com",
-//   databaseURL: "https://todo-app-381c4.firebaseio.com",
-//   projectId: "todo-app-381c4",
-//   storageBucket: "todo-app-381c4.appspot.com",
-//   messagingSenderId: "779022608299",
-//   appId: "1:779022608299:web:04c96725667d168e5894ee",
-//   measurementId: "G-BYPQ2T8EGP",
-// };
-
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAPifTJth3vw1KRugk2vudpYQB4cNezQQs",
-  authDomain: "todo-app-381c4.firebaseapp.com",
+  apiKey: "Rugk2vudpYQB4cNezQQs",
+  authDomain: "todo-app-3seapp.com",
   databaseURL: "https://todo-app-381c4.firebaseio.com",
   projectId: "todo-app-381c4",
   storageBucket: "todo-app-381c4.appspot.com",
   messagingSenderId: "779022608299",
-  appId: "1:779022608299:web:04c96725667d168e5894ee",
-  measurementId: "G-BYPQ2T8EGP",
+  appId: "1:779022608225667d168e5894ee",
+  measurementId: "G-BYEGP",
 });
 
 const db = firebaseApp.firestore();
